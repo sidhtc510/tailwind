472fc3a351ecc2c131dfd0744af3772ec7411715
sidhtc510 <sidhtc510@gmail.com>
2022-02-11 21:05:30 +0300

    first commit

---
+++ src/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://fonts.googleapis.com/css2?family=Nunito&display=swap" rel="stylesheet">
+
+    <!-- <script src="https://cdn.tailwindcss.com"></script> -->
+    <link href="/src/styles/output.css" rel="stylesheet">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="appp">
+        <h3>test</h3>
+        <a href="ap.php">ap link</a>
+    </div>
+</body>
+
+</html>
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: ["./src/**/*.{html,php,js}"],
+    prefix: 'tw-',
+    fontFamily: {
+        sans: ['Graphik', 'sans-serif'],
+        serif: ['Merriweather', 'serif'],
+    },
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
